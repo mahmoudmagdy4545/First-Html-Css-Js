2987acbeb33c6190462de19a2311d49f2dd02978
mahmoudmagdy4545 <mahmoudmagdy45456@gmail.com>
2024-09-23 20:27:49 +0300

    code

---
+++ main.js
@@ -0,0 +1,34 @@
+let stars=document.getElementById("stars");
+let moon=document.getElementById("moon");
+let mountains3=document.getElementById("mountains3");
+let mountains4=document.getElementById("mountains4");
+let river=document.getElementById("river");
+let boat=document.getElementById("boat");
+let nouvil=document.querySelector(".Nouvil");
+window.onscroll=function(){
+    let value=scrollY;
+    stars.style.left=value+'px';
+    moon.style.top=value*3+'px';
+    mountains3.style.top=value*2+'px';
+    mountains4.style.top=value*1.5+'px';
+    river.style.top=value+'px';
+    boat.style.top=value+'px';
+    boat.style.left=value+'px';
+
+    if(scrollY < 70){
+    nouvil.style.fontSize=value +'px';
+    nouvil.style.position='fixed';
+    } 
+    if(scrollY >= 470){
+        nouvil.style.display='none';
+    }
+    else{
+        nouvil.style.display='block';
+    } 
+    if(scrollY>=170){
+        document.querySelector(".main").style.background='linear-gradient(#376281,#10001f)';
+    }  
+    else{
+        document.querySelector(".main").style.background='linear-gradient(#200016,#10001f)';
+    }
+}
